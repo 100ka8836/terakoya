a8d1ce36cd9245d1d5843d43707a435fef419133
100ka8836 <151914993+100ka8836@users.noreply.github.com>
2024-07-06 22:01:02 +0900

    Add files via upload

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>確認画面</title>
+</head>
+<body>
+    <h1>確認画面</h1>
+    <?php
+        $employee_name = $_POST['employee_name'];
+        $employee_age = $_POST['employee_age'];
+        $department = $_POST['department'];
+    ?>
+    <table border="1">
+        <tr>
+            <th>項目</th>
+            <th>内容</th>
+        </tr>
+        <tr>
+            <td>社員名</td>
+            <td><?php echo htmlspecialchars($employee_name, ENT_QUOTES, 'UTF-8'); ?></td>
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo htmlspecialchars($employee_age, ENT_QUOTES, 'UTF-8'); ?></td>
+        </tr>
+        <tr>
+            <td>所属部署</td>
+            <td><?php echo htmlspecialchars($department, ENT_QUOTES, 'UTF-8'); ?></td>
+        </tr>
+    </table>
+    <form action="complete.php" method="post" style="display:inline;">
+        <input type="hidden" name="employee_name" value="<?php echo htmlspecialchars($employee_name, ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="hidden" name="employee_age" value="<?php echo htmlspecialchars($employee_age, ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="hidden" name="department" value="<?php echo htmlspecialchars($department, ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="submit" value="確定">
+    </form>
+    <form action="form.php" method="post" style="display:inline;">
+        <input type="submit" value="キャンセル">
+    </form>
+</body>
+</html>
